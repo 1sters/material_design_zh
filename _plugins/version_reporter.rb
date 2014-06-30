5b39acc20b2cf993f1559dd2863fc77921b4b239
iceskysl <iceskysl@gmail.com>
2014-06-30 11:46:27 +0800

    add plugin

---
+++ _plugins/version_reporter.rb
@@ -0,0 +1,26 @@
+# An example Jekyll generator. Utilizes the new plugin system.
+#
+# 1. Make a _plugins directory in your jekyll site, and put this class in a file there.
+# 2. Upon site generation, version.html will be created in your root destination with
+# #  the version of Jekyll that generated it
+ 
+module Jekyll
+  class VersionReporter < Generator
+    safe true
+ 
+    def generate(site)
+      puts "---------generate----#{File.join(site.source, 'version.html')}---"
+      File.open(File.join(site.source, 'version.html'), 'w') do |f|
+        f.write(generate_report(site))
+      end
+    end
+ 
+    private
+ 
+    def generate_report(site)
+      puts "---------generate_report---#{Jekyll::VERSION}}----"
+      "Site generated with Jekyll version: #{Jekyll::VERSION}"
+    end
+ 
+  end
+end
